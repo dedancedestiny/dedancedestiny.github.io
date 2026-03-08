6094ed46dad01cf971b29b2d7812bdae9bc6fd7b
dedancedestiny <omrdedancedestiny@gmail.com>
2026-03-08 21:13:08 +0530

    read from csv

--- src/utils/loadGalleryData.js
+++ src/utils/loadGalleryData.js
@@ -1,7 +1,7 @@
 import Papa from "papaparse";
 
 export const loadGalleryData = async () => {
-  const response = await fetch("/src/data/hero-gallery.csv");
+  const response = await fetch("/data/hero-gallery.csv");
   const csvText = await response.text();
 
   return new Promise((resolve) => {
